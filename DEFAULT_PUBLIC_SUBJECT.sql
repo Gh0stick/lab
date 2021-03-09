2d6996fa87f8cea44c1e6bf40d5534dce85ba350
DESKTOP-CPRIO14\asass <asassin99@mail.ru>
2021-03-09 23:22:45 +0300

    Commit
    Signed-off-by: DESKTOP-CPRIO14\asass <asassin99@mail.ru>

--- DEFAULT_PUBLIC_SUBJECT.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO PUBLIC.SUBJECT (ID, NAME, SHORT_NAME) VALUES (1, 'Проектирование информационных систем', 'ПрИС');
-INSERT INTO PUBLIC.SUBJECT (ID, NAME, SHORT_NAME) VALUES (2, 'Системы искусственного интеллекта', 'СИИ');
-INSERT INTO PUBLIC.SUBJECT (ID, NAME, SHORT_NAME) VALUES (3, 'Программная инженерия', 'ПИ');
-INSERT INTO PUBLIC.SUBJECT (ID, NAME, SHORT_NAME) VALUES (4, 'Национальная система информационной безопасности', 'НСИБ');
-INSERT INTO PUBLIC.SUBJECT (ID, NAME, SHORT_NAME) VALUES (5, 'Системный анализ', 'СисАнал');
-INSERT INTO PUBLIC.SUBJECT (ID, NAME, SHORT_NAME) VALUES (6, 'Распределенные базы данных', 'РБД');
-INSERT INTO PUBLIC.SUBJECT (ID, NAME, SHORT_NAME) VALUES (7, 'Системное программное обеспечение', 'СПО');
